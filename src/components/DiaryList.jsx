51a67d43899944941fb3b9d2059e578e84766b76
laurenCho9 <lauren.choux@gmail.com>
2024-01-21 22:56:57 +0900

    feat: 데이터 수정하기

--- src/components/DiaryList.jsx
+++ src/components/DiaryList.jsx
@@ -1,15 +1,24 @@
 import DiaryItem from "./DiaryItem";
 
-export default function DiaryList({ diaryList, onRemove }) {
+export default function DiaryList({ diaryList, onRemove, onEdit }) {
   return (
     <div className="diary-list">
       <h2>일기 리스트</h2>
       <h4>{diaryList.length}개의 일기가 있습니다.</h4>
       <div>
         {diaryList.map((it) => (
-          <DiaryItem key={`diaryitem_${it.id}`} {...it} onRemove={onRemove} />
+          <DiaryItem
+            key={`diaryitem_${it.id}`}
+            {...it}
+            onRemove={onRemove}
+            onEdit={onEdit}
+          />
         ))}
       </div>
     </div>
   );
 }
+
+// DiaryList.defaultProps = {
+//   diaryList: []
+// };
